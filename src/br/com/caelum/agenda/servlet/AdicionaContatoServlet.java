a12dd3f921ac29a1865ba4ddef09fd0bfc805f86
web7302 <web7302@caelum57-06>
2018-03-31 12:41:04 -0300

    Classes DAO/Modelo

---
+++ src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,45 @@
+package br.com.caelum.agenda.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdicionaContatoServlet
+ */
+@WebServlet("/AdicionaContatoServlet")
+public class AdicionaContatoServlet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nome");
+		String endereco =  request.getParameter("endereco");
+		String email = request.getParameter("email");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+		
+		Date date;
+		try {
+			date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+		} catch (ParseException e) {
+			out.println("Erro de conversao de data");
+			return;
+		}
+		
+		Contato contato = new Contato();
+		
+
+	}
+
+}
