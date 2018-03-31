29ef4a2615e16e138f6670b09e6ff45160ab7431
web7302 <web7302@caelum57-06>
2018-03-31 14:58:20 -0300

    Novos Fontes

--- src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
+++ src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -13,33 +13,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.caelum.agenda.dao.ContatoDao;
+import br.com.caelum.agenda.modelo.Contato;
+
 /**
  * Servlet implementation class AdicionaContatoServlet
  */
-@WebServlet("/AdicionaContatoServlet")
+@WebServlet("/adicionaContato")
 public class AdicionaContatoServlet extends HttpServlet {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 		PrintWriter out = response.getWriter();
 		String nome = request.getParameter("nome");
-		String endereco =  request.getParameter("endereco");
+		String endereco = request.getParameter("endereco");
 		String email = request.getParameter("email");
 		String dataEmTexto = request.getParameter("dataNascimento");
 		Calendar dataNascimento = null;
-		
-		Date date;
+
 		try {
-			date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
 			dataNascimento = Calendar.getInstance();
 			dataNascimento.setTime(date);
 		} catch (ParseException e) {
 			out.println("Erro de conversao de data");
 			return;
 		}
-		
-		Contato contato = new Contato();
-		
 
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEndereco(endereco);
+		contato.setEmail(email);
+		contato.setDataNascimento(dataNascimento);
+
+		ContatoDao dao = new ContatoDao();
+		dao.adiciona(contato);
+
+		out.print("<html>");
+		out.print("<body>");
+		out.print("Contato " + contato.getNome() + " adicionado com sucesso");
+		out.print("</html>");
+		out.print("</body>");
 	}
 
 }
